99981c2483d1786dca560425321167a45a6d7e51
pitonmarquise <pitonmarquise@gmail.com>
2021-05-03 21:35:26 -0400

    add more apis for the 3 players

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -7,32 +7,94 @@ use Illuminate\Support\Facades\DB;
 
 class DataController extends Controller
 {
-    
+    // =================================== General Stats Controllers ===============================================
     public function postseasonaverages()
     {
         $postseasonaverages = DB::table('postseasonaverages')->get();
-
         return $postseasonaverages;
     }
     public function postseasontotals()
     {
         $postseasontotals = DB::table('postseasontotals')->get();
-
         return $postseasontotals;
     }
     public function regularseasonaverages()
     {
         $regularseasonaverages = DB::table('regularseasonaverages')->get();
-
         return $regularseasonaverages;
     }
     public function regularseasontotals()
     {
         $regularseasontotals = DB::table('regularseasontotals')->get();
-
         return $regularseasontotals;
     }
-    
-    
+    // ==============================================================================================================
+    // ==================================== Lebron Stats Controllers ================================================
+    public function lebronregularseasonaverages()
+    {
+        $lebronsregularseasonaverage = DB::table('regularseasonaverages')->where('player','1')->get();
+        return $lebronsregularseasonaverage; 
+    }
+    public function lebronregularseasontotals()
+    {
+        $lebronsregularseasontotals = DB::table('regularseasontotals')->where('player','1')->get();
+        return $lebronsregularseasontotals;
+        
+    }
+    public function lebronpostseasonaverages()
+    {
+      $lebronpostseasonaverages = DB::table('postseasonaverages')->where('player','1')->get();
+        return $lebronpostseasonaverages;
+    }  
+    public function lebronpostseasontotals()
+    {
+        $lebronpostseasontotals = DB::table('postseasontotals')->where('player','1')->get();
+        return $lebronpostseasontotals;
+    }
+    // ==============================================================================================================
+    // =================================== Kobe Stats Conrollers ====================================================
+     public function koberegularseasonaverages()
+    {
+        $koberegularseasonaverage = DB::table('regularseasonaverages')->where('player','2')->get();
+        return $koberegularseasonaverage; 
+    }
+    public function koberegularseasontotals()
+    {
+        $koberegularseasontotals = DB::table('regularseasontotals')->where('player','2')->get();
+        return $koberegularseasontotals;
+    }
+    public function kobepostseasonaverages()
+    {
+      $kobepostseasonaverages = DB::table('postseasonaverages')->where('player','2')->get();
+        return $kobepostseasonaverages;
+    }  
+    public function kobepostseasontotals()
+    {
+        $kobepostseasontotals = DB::table('postseasontotals')->where('player','2')->get();
+        return $kobepostseasontotals;
+    }
+    // ==============================================================================================================
+    // =================================== Micheal Stats Conrollers ====================================================
+    public function michealregularseasonaverages()
+    {
+        $michealregularseasonaverage = DB::table('regularseasonaverages')->where('player','3')->get();
+        return $michealregularseasonaverage; 
+    }
+    public function michealregularseasontotals()
+    {
+        $michealregularseasontotals = DB::table('regularseasontotals')->where('player','3')->get();
+        return $michealregularseasontotals;
+    }
+    public function michealpostseasonaverages()
+    {
+      $michealpostseasonaverages = DB::table('postseasonaverages')->where('player','3')->get();
+        return $michealpostseasonaverages;
+    }  
+    public function michealpostseasontotals()
+    {
+        $michealpostseasontotals = DB::table('postseasontotals')->where('player','3')->get();
+        return $michealpostseasontotals;
+    }
+    // ================================================================================================================
     
 }
--- routes/api.php
+++ routes/api.php
@@ -26,41 +26,69 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 // User CRUD
-//Create
+//Create new user
 Route::post('/register',[UserController::class,'register']);
-
 // Read 
 Route::get('/users/all',[UserController::class, 'index']);
-
-// Update
+// Update User
 Route::post('/user/{id}/update',[UserController::class,'update']);
-
-// Delete
+// Delete User
 Route::get('/user/{id}/delete',[UserController::class,'destroy']);
-
-
 // Team CRUD
-
-// Read 
+// This is for register page 
 Route::get('/team/all',[TeamController::class, 'index']);
-
-
 // Player CRUD
+// This is for register page 
 Route::get('/player/all',[PlayerController::class,'index']);
 
+// ================================ General ======================================================================
 // Get postseason average
-Route::get('/stats/post-season-averages',[DataController::class, 'postseasonaverages']);
-
-
+Route::get('/stats/postseason-averages',[DataController::class, 'postseasonaverages']);
 // Get postseason total
-Route::get('/stats/post-season-totals',[DataController::class, 'postseasontotals']);
-
-
+Route::get('/stats/postseason-totals',[DataController::class, 'postseasontotals']);
 // Get regular season averages 
 Route::get('/stats/regular-season-averages',[DataController::class, 'regularseasonaverages']);
-
 // Get regular season totals
 Route::get('/stats/regular-season-totals',[DataController::class, 'regularseasontotals']);
+// ===============================================================================================================
+
+
+//======================================LEBRON======================================================
+// Regular season average 
+Route::get('/stats/lebron-regular-season-averages',[DataController::class, 'lebronsregularseasonaverages']);
+// Regular season total 
+Route::get('/stats/lebron-regular-season-totals',[DataController::class, 'lebronsregularseasontotals']);
+// Postseason average
+Route::get('/stats/lebron-postseason-averages', [DataController::class,'lebronpostseasonaverages']);
+// Postseason total
+Route::get('/stats/lebron-postseason-totals', [DataController::class,'lebronpostseasontotals']);
+// =================================================================================================
+
+
+// ==========================================KOBE===================================================
+// Regular season average 
+Route::get('/stats/kobe-regular-season-averages',[DataController::class, 'koberegularseasonaverages']);
+// Regular season total 
+Route::get('/stats/kobe-regular-season-totals',[DataController::class, 'koberegularseasontotals']);
+// Postseason average
+Route::get('/stats/kobe-postseason-averages', [DataController::class,'kobepostseasonaverages']);
+// Postseason total
+Route::get('/stats/kobe-postseason-totals', [DataController::class,'kobepostseasontotals']);
+// ==================================================================================================
+
+
+
+// ===========================================MICHEAL==============================================
+// Regular season average 
+Route::get('/stats/micheal-regular-season-averages',[DataController::class, 'michealregularseasonaverages']);
+// Regular season total 
+Route::get('/stats/micheal-regular-season-totals',[DataController::class, 'michealregularseasontotals']);
+// Postseason average
+Route::get('/stats/micheal-postseason-averages', [DataController::class,'michealpostseasonaverages']);
+// Postseason total
+Route::get('/stats/micheal-postseason-totals', [DataController::class,'michealpostseasontotals']);
+// ==================================================================================================
+
 
 
 
